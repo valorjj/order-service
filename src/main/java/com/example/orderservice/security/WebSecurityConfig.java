55f20c8502c723469e295c9cb4e6a11b13076df4
Jeongjin Kim <valorjj@gmail.com>
2023-10-20 23:46:12 +0900

    csrf disabled

--- src/main/java/com/example/orderservice/security/WebSecurityConfig.java
+++ src/main/java/com/example/orderservice/security/WebSecurityConfig.java
@@ -2,10 +2,10 @@ package com.example.orderservice.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.*;
@@ -19,7 +19,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
         http.oauth2ResourceServer(auth -> auth.jwt(withDefaults()));
-
+        http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
