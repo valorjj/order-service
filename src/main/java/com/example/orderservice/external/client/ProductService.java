c9ee0ef7d8b264a2f9f0f3800e4db37a4f7d44c3
Jeongjin Kim <valorjj@gmail.com>
2023-10-24 15:05:11 +0900

    Update ProductService.java

--- src/main/java/com/example/orderservice/external/client/ProductService.java
+++ src/main/java/com/example/orderservice/external/client/ProductService.java
@@ -2,9 +2,11 @@ package com.example.orderservice.external.client;
 
 
 import com.example.orderservice.exception.CustomException;
+import com.example.orderservice.external.model.ProductResponse;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +19,11 @@ public interface ProductService {
     ResponseEntity<Integer> reduceQuantity(
         @PathVariable("id") Long productId,
         @RequestParam Long quantity
+    );
+
+    @GetMapping("/{id}")
+    ResponseEntity<ProductResponse> getProductById(
+        @PathVariable("id") Long productId
     );
 
     default ResponseEntity<Void> fallback(Exception e) {
